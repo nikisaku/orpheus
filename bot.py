75c40cd0363ad0351c02191ed252aec461be1c4b
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2020-12-28 19:40:58 +0100

    Add multiple trivia sources

--- bot.py
+++ bot.py
@@ -20,23 +20,25 @@ with open('themes.csv') as f:
     for line in csv_reader:
         advent_calendar[line[0]] = line[1]
 
-with open('webaccesibility.csv') as f:
-    csv_reader = csv.reader(f)
-    for line in csv_reader:
-        trivia[line[0]] = line[1]
+files_to_read = ['webaccessibility.csv', 'photography.csv']
+for file_to_read in files_to_read:
+    with open(file_to_read) as f:
+        csv_reader = csv.reader(f)
+        for line in csv_reader:
+            trivia[line[0]] = line[1]
 
 
 channel_ids = []
 trivia_channel_ids = []
 
-@aiocron.crontab('0 5 * * *')
+@aiocron.crontab('15 5 * * *')
 async def cronjob1():
     today = datetime.date.today().strftime('%Y-%m-%d')
     theme = advent_calendar[today]
     for channel_id in channel_ids:
-        await client.get_channel(channel_id).send(f"Dzisiejszy temat to {theme}.")
+        await client.get_channel(channel_id).send(f"Dzisiejszy temat to: {theme}")
 
-@aiocron.crontab('0 8 * * *')
+@aiocron.crontab('0 7 * * *')
 async def cronjob2():
     today = datetime.date.today().strftime('%Y-%m-%d')
     trivia_of_the_day = trivia[today]
@@ -49,9 +51,10 @@ async def on_ready():
     for guild in client.guilds:
         print(f'{client.user} has connected to Discord server {guild}!')
         for channel in guild.channels:
-            if 'music' in channel.name:
-                channel_ids.append(channel.id)
-            if 'ciekawostka-dnia' in channel.name:
-                trivia_channel_ids.append(channel.id)
+            if isinstance(channel, discord.TextChannel):
+                if 'music' in channel.name:
+                    channel_ids.append(channel.id)
+                if 'ciekawostka-dnia' in channel.name:
+                    trivia_channel_ids.append(channel.id)
 
 client.run(TOKEN)
